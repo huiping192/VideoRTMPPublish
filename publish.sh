b572b824ebdb2d5a73d6044ea0a90671a0ad57be
huiping_guo <huiping.guo@dena.jp>
2022-12-28 20:31:41 +0900

    video file support

--- publish.sh
+++ publish.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
 
 while true
 do
 	cd /video
-	for video in $(ls *.mp4)
+	for video in $(ls)
 	do
 		ffmpeg -re -i "$video" -preset ultrafast -vcodec libx264 -g 60 -b:v 6000k -c:a aac -b:a 128k -strict -2 -f flv "rtmp://192.168.11.1:1935/live/test"
 	done
